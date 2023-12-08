d6edfc03529ec6e97e2ec91062f32c7047ffe085
Breno Reis <breu1234@gmail.com>
2023-12-08 13:58:10 -0300

    feat: first lesson

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Movie myMovie = new Movie();
+        myMovie.name = "Guardians of the Galaxy";
+        myMovie.releaseYear = 2013;
+        myMovie.minutesLength = 180;
+
+        myMovie.showDetails();
+        myMovie.rate(8);
+        myMovie.rate(5);
+        myMovie.rate(10);
+        System.out.println(myMovie.ratings);
+        System.out.println(myMovie.totalRatings);
+        System.out.println(myMovie.getAverageRating());
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Movie {
+public class Movie {
+    String name;
+    int releaseYear;
+    boolean includedInThePlan;
+    double ratings;
+    int totalRatings;
+    int minutesLength;
+
+    void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Release Year: " + releaseYear);
+    }
+
+    void rate(double score) {
+        ratings += score;
+        totalRatings++;
+    }
+
+    double getAverageRating() {
+        return ratings/totalRatings;
+    }
 }
